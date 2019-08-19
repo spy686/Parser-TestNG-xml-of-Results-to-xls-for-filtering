03e3130fbf099f61a1827685ecc7f8cd12bcd14c
YauheniPo <ip.popovich@mail.ru>
2019-08-19 21:30:00 +0300

    mvn clean package for getting runnable jar file

--- src/main/java/RunTestNGResultsParserToXls.java
+++ src/main/java/RunTestNGResultsParserToXls.java
@@ -1,3 +1,5 @@
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -23,7 +25,8 @@ public class RunTestNGResultsParserToXls {
     private static final By failedTestsStacktracesLocator = By.xpath("//h2[@id]//following-sibling::*//div[@class='stacktrace']");
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String projectDir = URLDecoder.decode(System.getProperty("user.dir") + File.separator, "UTF-8");
+        String projectDir = URLDecoder.decode(new File(RunTestNGResultsParserToXls.class.getProtectionDomain().getCodeSource().getLocation()
+                .getPath()).getPath() + File.separator, "UTF-8");
 
         JFileChooser jFileChooser = new JFileChooser();
         jFileChooser.setDialogTitle("Choose a report");
@@ -31,22 +34,22 @@ public class RunTestNGResultsParserToXls {
         jFileChooser.showOpenDialog(null);
         File file = jFileChooser.getSelectedFile();
 
-        System.setProperty("webdriver.chrome.driver", projectDir + "chromedriver.exe");
-
         //открываем репорт в браузере и покируем путь из строки поиска или просто путь к файлу
 //        String reportTestNGPath = "C:\\Users\\Xiaomi\\Google Диск\\popo\\java\\parse_testng_results\\report.html";
         String reportTestNGPath = file.getAbsolutePath();
-        String fileNameXls = FilenameUtils.removeExtension(new File(reportTestNGPath).getName()) + ".xlsx";
 
+        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         WebDriver driver = new ChromeDriver(options);
         driver.get(reportTestNGPath);
 
         List<String> failedTestsNames = driver.findElements(failedTestsNamesLocator).stream().map(WebElement::getText).collect(Collectors.toList());
-        System.out.println(String.format("Was found %d tests names", failedTestsNames.size()));
+        int failedTestsNamesCount = failedTestsNames.size();
+        System.out.println(String.format("Was found %d tests names", failedTestsNamesCount));
         List<String> failedTestsStacktraces = driver.findElements(failedTestsStacktracesLocator).stream().map(WebElement::getText).collect(Collectors.toList());
-        System.out.println(String.format("Was found %d tests stacktraces", failedTestsStacktraces.size()));
+        int failedTestsStacktracesCount = failedTestsStacktraces.size();
+        System.out.println(String.format("Was found %d tests stacktraces", failedTestsStacktracesCount));
 
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("report");
@@ -59,10 +62,16 @@ public class RunTestNGResultsParserToXls {
             cell2.setCellValue(failedTestsStacktraces.get(rowNum));
         }
 
-        try (FileOutputStream out = new FileOutputStream(new File(projectDir + fileNameXls))) {
+        String fileNameXls = String.format("%s_%dTs_%dStrs.%s",
+                FilenameUtils.removeExtension(new File(reportTestNGPath).getName()) + File.separator,
+                failedTestsNamesCount,
+                failedTestsStacktracesCount,
+                "xlsx");
+        try (FileOutputStream out = new FileOutputStream(new File(new File(projectDir).getParent() + fileNameXls))) {
             workbook.write(out);
         } catch (Exception e) {
             e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
 
         driver.quit();
